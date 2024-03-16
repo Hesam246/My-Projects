dc44c01e4a53122c6e2f9f7bc90a3a675298a2ae
Hesam246 <163550338+Hesam246@users.noreply.github.com>
2024-03-16 04:22:49 -0700

    Update main.py

--- DumpFile/main.py
+++ DumpFile/main.py
@@ -1,1 +1,16 @@
-ll pygame.freetype.was_init()orld!
+import os
+
+def create_file(size_in_b, file_name):
+    with open(file_name, 'wb') as f:
+        f.write(b'\0' * size_in_b)
+
+b = int(input("Enter the size of the file in bytes: "))
+mb = int(input("Enter the size of the file in megabytes: "))
+gb = int(input("Enter the size of the file in gigabytes: "))
+
+total_size = b + mb * 1024 * 1024 + gb * 1024 * 1024 * 1024
+
+file_name = "test_file.txt"
+create_file(total_size, file_name)
+
+print(f"File with size {total_size} bytes created successfully as {file_name}")
